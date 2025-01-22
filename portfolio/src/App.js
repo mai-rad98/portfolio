0d996cabbbdc030b8e7e4dcc39234eac21b1199b
MAITUMELO RADIPODI <mradipodi@gmail.com>
2025-01-22 06:10:44 +0200

    updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+Maitumelo's portfolio loading        </a>
       </header>
     </div>
   );
